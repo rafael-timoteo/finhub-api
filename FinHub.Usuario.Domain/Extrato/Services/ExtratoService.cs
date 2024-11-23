3da351041c1038be63c81491983de9c6396edcfb
gaaaasouza <gaaaasouza@gmail.com>
2024-11-23 16:14:03 -0300

    feat: criação serviço do usuário

---
+++ FinHub.Usuario.Domain/Extrato/Services/ExtratoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinHub.Usuario.Domain.Extrato.Services
+{
+    internal class ExtratoService
+    {
+    }
+}
